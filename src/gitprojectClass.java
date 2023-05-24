5e37ce4b6221bf63e5bde4e9de380ee975106d89
Piyush <piyushsust@yahoo.com>
2023-05-24 19:28:05 -0400

    hello

---
+++ src/gitprojectClass.java
@@ -0,0 +1,16 @@
+public class gitprojectClass {
+
+
+        public static void main(String[] args) {
+            Arraylist<String> names = new Arraylist<>();
+            names.add("Sofia");
+            names.add("Fatin");
+            names.add("Sarika");
+            names.add("Kashif");
+            names.add("Halim");
+            for (String name : names) {
+                System.out.println(name);
+
+            }
+        }
+}
